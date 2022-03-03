265daed54662833ad4d9a6b637e7e158df2fb485
Vitaly <w.182@mail.ru>
2022-03-03 03:13:53 +0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
-import {useState, useEffect, useRef, useMemo} from 'react'
+import {useState, useEffect, useRef, useMemo, useCallback} from 'react'
+import ItemsList from './itemsList'
 
-
-function complexComputed(num) {
-  console.log('complexComputed');
-  let i = 0
-  while (i < 1000000000) i++
-  return num * 2
-}
 
 function App() {
 
-  const [number, setNumber] = useState(42)
   const [colored, setColored] = useState(false)
+  const [count, setCount] = useState(1)
 
-  const styles = useMemo(() => {
-    return {
-      color: colored ? 'blue' : 'black'
-    }
-  }, [colored])
+  const styles = {
+    color: colored ? 'blue' : 'black'
+  }
 
-  const computed = useMemo(() => {
-    return complexComputed(number)
-  }, [number])
-
-  useEffect(() => {
-    console.log('Slyles changed')
-  }, [styles])
-
+  const generateItemsFromAPI = useCallback(() => {
+    return new Array(count).fill('').map((_, i) => `Элемент ${i + 1}`)
+  }, [count])
 
 
   return (
     <div>
-      <h1 style={styles}>Вычисляемое свойство: {computed}</h1>
-      <button className={'btn btn-success'} onClick={() => setNumber( prev => prev + 1)} >Добавить</button>
-      <button className={'btn btn-danger'} onClick={() => setNumber( prev => prev - 1)} >Убрать</button>
-      <button className={'btn btn-warning'} onClick={() => setColored( prev => !prev)} >Изменить</button>
+      <h1 style={styles}>Количество элементов: {count}</h1>
+      <button className={'btn btn-success'} onClick={() => setCount(prev => prev + 1)}>Добавить</button>
+      <button className={'btn btn-warning'} onClick={() => setColored(prev => !prev)}>Изменить</button>
+
+      <ItemsList getItems={generateItemsFromAPI} />
     </div>
   );
 }
